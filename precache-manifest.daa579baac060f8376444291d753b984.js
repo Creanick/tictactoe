335193f35e7494d25d77c76e458b5cf182b1000d
Manick <manick@Manicks-MacBook-Air.local>
2019-05-06 17:22:54 +0530

    Updates

---
+++ precache-manifest.daa579baac060f8376444291d753b984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3c5305fb36f80579962a6022417c92",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "be291e1611863fd42a24",
+    "url": "/tictactoe/static/css/main.52cfa900.chunk.css"
+  },
+  {
+    "revision": "8f8d4141f11d28fb2c0b",
+    "url": "/tictactoe/static/js/2.72c82199.chunk.js"
+  },
+  {
+    "revision": "be291e1611863fd42a24",
+    "url": "/tictactoe/static/js/main.13662b37.chunk.js"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  }
+]);
